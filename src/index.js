ab42f34ce34e4264bd18f084d02035ad9926b0a7
Dzianis_Shutikov <Dzianis_Shutikov@epam.com>
2018-09-23 22:21:01 +0300

    solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let strKey = str.length; strKey >= 0; strKey--) {
+    for (let bracketKey = 0; bracketKey < bracketsConfig.length; bracketKey++) {
+      if (str[strKey] === bracketsConfig[bracketKey][0]) {
+        str = str.replace(bracketsConfig[bracketKey].join(''), '');
+      }
+    }
+  }
+  return str == '';
 }
